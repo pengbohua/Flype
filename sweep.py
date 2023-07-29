08f630945f1454b92798d5573f095b298dbf8d38
Bohua Peng <marvinbp1119@gmail.com>
2023-07-29 16:22:50 +0100

    change names

--- sweep.py
+++ sweep.py
@@ -4,5 +4,6 @@ import yaml
 with open("sweep_flype.yaml", "r") as f:
     sweep_config = yaml.safe_load(f)
 
-sweep_id = wandb.sweep(sweep_config, entity="marvinpeng", project="blipopt")
+sweep_id = wandb.sweep(sweep_config, entity="marvinpeng", project="flype")
 print(sweep_id)
+
